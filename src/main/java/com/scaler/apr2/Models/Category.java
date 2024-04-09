06d42dd82c324b0c38d3b1761cea1a0af61566b6
NareshBiradar1 <nareshdb555@gmail.com>
2024-04-09 09:36:41 +0530

    Added own database

--- src/main/java/com/scaler/apr2/Models/Category.java
+++ src/main/java/com/scaler/apr2/Models/Category.java
@@ -1,12 +1,15 @@
 package com.scaler.apr2.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
-
+    @Id
     private Long id;
 
     private String name ;
--- src/main/java/com/scaler/apr2/Models/Product.java
+++ src/main/java/com/scaler/apr2/Models/Product.java
@@ -1,19 +1,25 @@
 package com.scaler.apr2.Models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
 
+    @Id
     private Long id;
 
     private String title;
 
     private double Price;
 
+    @ManyToOne
     private Category category ;
 
     private String description;
